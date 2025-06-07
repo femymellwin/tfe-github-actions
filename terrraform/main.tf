044ab927901de105baf2eb1337c8e13aa3f31448
femymellwin <54503706+femymellwin@users.noreply.github.com>
2025-06-07 15:38:15 +0530

    Update main.tf

--- terrraform/main.tf
+++ terrraform/main.tf
@@ -2,6 +2,12 @@ provider "aws" {
   region = var.region
 }
 
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
 resource "aws_instance" "test_vm" {
   ami           = var.ami_id
   instance_type = var.instance_type
