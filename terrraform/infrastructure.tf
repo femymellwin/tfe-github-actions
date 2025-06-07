bb93929afca51b1847c2b0774c7910f1f212eb54
femymellwin <54503706+femymellwin@users.noreply.github.com>
2025-06-07 14:11:06 +0530

    Update infrastructure.tf

--- terrraform/infrastructure.tf
+++ terrraform/infrastructure.tf
@@ -1,17 +1,1 @@
-resource "aws_s3_bucket" "tf_state_bucket" {
-  bucket = "your-unique-terraform-state-bucket-2025-06-07"  # Change to unique globally
-  acl    = "private"
 
-  tags = {
-    Name        = "TerraformStateBucket"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket_versioning" "tf_state_bucket_versioning" {
-  bucket = aws_s3_bucket.tf_state_bucket.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
