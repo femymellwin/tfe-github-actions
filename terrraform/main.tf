a294b85ff6c09295e2e7b2c40ece90ccf066772a
femymellwin <54503706+femymellwin@users.noreply.github.com>
2025-06-07 15:13:45 +0530

    Update main.tf

--- terrraform/main.tf
+++ terrraform/main.tf
@@ -2,4 +2,13 @@ provider "aws" {
   region = var.region
 }
 
+resource "aws_instance" "test_vm" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "GitHub-Actions-VM1"
+  }
+
+}
 
