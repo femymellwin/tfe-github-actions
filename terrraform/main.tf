e5e579d1ca228b185220e004757e4186955db867
femymellwin <54503706+femymellwin@users.noreply.github.com>
2025-06-07 19:01:47 +0530

    Update main.tf

--- terrraform/main.tf
+++ terrraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "your-s3-bucket-name"
+    bucket         = "fmshr-tfe-statefile-bucket"
     key            = "env/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock-table"
