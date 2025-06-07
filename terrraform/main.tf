b566fc3d06d51f1284a8769bf68bb085c180cfb7
femymellwin <54503706+femymellwin@users.noreply.github.com>
2025-06-07 19:01:18 +0530

    Update main.tf

--- terrraform/main.tf
+++ terrraform/main.tf
@@ -3,10 +3,15 @@ provider "aws" {
 }
 
 terraform {
-  backend "local" {
-    path = "terraform.tfstate"
+  backend "s3" {
+    bucket         = "your-s3-bucket-name"
+    key            = "env/dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
   }
 }
+
 
 resource "aws_instance" "test_vm" {
   ami           = var.ami_id
